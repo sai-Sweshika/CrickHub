b2c93bcf1876daa34a45118fae1c9fee228f13eb
sai-Sweshika <saisweshikakodurupaka@gmail.com>
2025-03-04 15:39:14 +0530

    first commit

---
+++ src/Components/CrickScore.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const CricketScore = () => {
+    return {
+        <div className='bg-gradient-to-b from-blue-500 to-indigo-600 max-w-screen-xl h-screen' >
+            <div className='flex items-center justufy-center'>
+                <input type="text" className="w-[60%] p-4 border-none" />
+
+            </div>
+        </div>
+
+    };
+}
+
+export default CricketScore
